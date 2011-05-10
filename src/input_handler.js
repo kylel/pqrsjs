0ac1d3e36d3f8a5b8a7db6f8699d1bf8a662be16
Kyle Ludwig <kyle.ludwig@gmail.com>
2011-05-10 16:08:39 +0200

    added mouse functionality to input handler

--- src/input_handler.js
+++ src/input_handler.js
@@ -3,9 +3,11 @@
   * 
   */
 
-InputHandler = function ()
+InputHandler = function (canvas)
 {
     this.clients = [];
+	this._mouse = {x:0,y:0};
+	this._canvas = canvas;
     start_input_handler(this);
 }
 
@@ -22,7 +24,7 @@ InputHandler.prototype.register = function (client)
 /**
  * 
  */
-InputHandler.prototype.change_key = function ()
+InputHandler.prototype._change_key = function (key, state)
 {
     for (client in this.clients)
     {
@@ -30,9 +32,38 @@ InputHandler.prototype.change_key = function ()
     }
 }
 
+/**
+ * 
+ */
+InputHandler.prototype._mouse_move = function (x,y)
+{
+	this._mouse.x = x;
+	this._mouse.y = y;
+}
+
+/**
+ * 
+ */
+InputHandler.prototype.get_mouse_position = function ()
+{
+	return {x:this._mouse.x,
+	        y:this._mouse.y};
+}
+
+/**
+ * 
+ */
+InputHandler.prototype.get_mouse_position_canvas = function ()
+{
+	return {x:this._mouse.x-this._canvas.offsetLeft,
+	        y:this._mouse.y-this._canvas.offsetTop};
+}
+
 function start_input_handler(input_handler)
 {
-    document.onkeydown=function(e){input_handler.change_key((e||window.event).keyCode, 1);}
-    document.onkeyup=function(e){input_handler.change_key((e||window.event).keyCode, 0);}
+    document.onkeydown=function(e){input_handler._change_key((e||window.event).keyCode, 1);}
+    document.onkeyup=function(e){input_handler._change_key((e||window.event).keyCode, 0);}
+	
+	document.addEventListener("mousemove", function(e) {input_handler._mouse_move((e||window.event).clientX, (e||window.event).clientY);} ,false);
 }
 
